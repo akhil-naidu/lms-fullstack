450de6724ef672b5eb36690ab8b06724e673bf8f
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-11-26 14:03:56 +0530

    chore: with https

--- server/app.ts
+++ server/app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ['lms-fullstack-production.up.railway.app'],
+    origin: ['https://lms-fullstack-production.up.railway.app'],
     credentials: true,
   }),
 );
