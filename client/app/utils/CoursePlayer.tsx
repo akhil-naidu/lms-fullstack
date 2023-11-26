92c9c38d4c2e265b893f5ec43d56128356b9945d
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-11-26 13:53:26 +0530

    updating url

--- client/app/utils/CoursePlayer.tsx
+++ client/app/utils/CoursePlayer.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useEffect, useState } from "react";
-import axios from "axios";
+import React, { FC, useEffect, useState } from 'react';
+import axios from 'axios';
 
 type Props = {
   videoUrl: string;
@@ -8,35 +8,40 @@ type Props = {
 
 const CoursePlayer: FC<Props> = ({ videoUrl }) => {
   const [videoData, setVideoData] = useState({
-    otp: "",
-    playbackInfo: "",
+    otp: '',
+    playbackInfo: '',
   });
 
   useEffect(() => {
     axios
-      .post("https://elearning-lms-7728bbd846c2.herokuapp.com/api/v1/getVdoCipherOTP", {
-        videoId: videoUrl,
-      })
+      .post(
+        'https://boorish-thought-production.up.railway.app/api/v1/getVdoCipherOTP',
+        {
+          videoId: videoUrl,
+        },
+      )
       .then((res) => {
         setVideoData(res.data);
       });
   }, [videoUrl]);
 
   return (
-    <div style={{position:"relative",paddingTop:"56.25%",overflow:"hidden"}}>
-      {videoData.otp && videoData.playbackInfo !== "" && (
+    <div
+      style={{ position: 'relative', paddingTop: '56.25%', overflow: 'hidden' }}
+    >
+      {videoData.otp && videoData.playbackInfo !== '' && (
         <iframe
           src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=Ub9OiZXIOeUXH0Nv`}
           style={{
-            position: "absolute",
+            position: 'absolute',
             top: 0,
             left: 0,
-            width: "100%",
-            height: "100%",
-            border: 0
+            width: '100%',
+            height: '100%',
+            border: 0,
           }}
           allowFullScreen={true}
-          allow="encrypted-media"
+          allow='encrypted-media'
         ></iframe>
       )}
     </div>
